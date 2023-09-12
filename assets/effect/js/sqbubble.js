72a326fae148c282321873f83afacc1271ca2e05
wpiraniir <144112383+wpiraniir@users.noreply.github.com>
2023-09-12 23:01:40 +0330

    Add files via upload

---
+++ assets/effect/js/sqbubble.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function(){
+jQuery( "<div class='area'><ul class='clp-shape'><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul></div>" ).appendTo( 'html' );
+});
